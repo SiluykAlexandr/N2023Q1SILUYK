54b181b6fe17a5e17439b36e93e52d35fb79ca9d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-10-27 11:15:57 +0300

    add: variable.js and weekdaySearch.js

---
+++ variable.js
@@ -0,0 +1,14 @@
+const jnumber = 7
+const hnumber =8
+const result = jnumber + hnumber
+const fword = "привет"
+const sword = "миp"
+const twoword = fword + sword
+console.log(result)
+console.log(twoword)
+
+
+
+
+
+
---
+++ weekDaySearch.js
@@ -0,0 +1,22 @@
+const numericRepresentationOfTheDayOfTheWeek = 3
+
+if (numericRepresentationOfTheDayOfTheWeek === 1)
+    console.log(`${ numericRepresentationOfTheDayOfTheWeek } - Это Понедельник`)
+else if (numericRepresentationOfTheDayOfTheWeek === 2)
+    console.log(`${ numericRepresentationOfTheDayOfTheWeek } - Это Вторник`)
+else if (numericRepresentationOfTheDayOfTheWeek === 3)
+    console.log(`${ numericRepresentationOfTheDayOfTheWeek } - Это Среда`)
+else if (numericRepresentationOfTheDayOfTheWeek === 4)
+    console.log(`${ numericRepresentationOfTheDayOfTheWeek } - Это Четверг`)
+else if (numericRepresentationOfTheDayOfTheWeek === 5)
+    console.log(`${ numericRepresentationOfTheDayOfTheWeek } - Это Пятница`)
+else if (numericRepresentationOfTheDayOfTheWeek === 6)
+    console.log(`${ numericRepresentationOfTheDayOfTheWeek } - Это Суббота`)
+else if (numericRepresentationOfTheDayOfTheWeek === 7)
+    console.log(`${ numericRepresentationOfTheDayOfTheWeek } - Это Воскресенье`)
+else console.log('неверные данные')
+
+
+
+
+
